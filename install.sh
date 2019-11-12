9ff2b3e1bc50532a081358f3db0443a9d28e3d25
jacobcainmontgomery <jacobcainmontgomery@gmail.com>
2019-11-12 17:04:15 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo -e "\e[92m[+]\e[0m Checking for AEScrypt"
+if ls -l /usr/bin/aescrypt | egrep -qi "x ";
+then
+        echo -e "\e[92m[+]\e[0m AESCrypt is already installed and executable"
+else
+        echo -e "\e[92m[+]\e[0m Not found. Installing."
+        sleep 3
+        wget https://www.aescrypt.com/download/v3/linux/AESCrypt-GUI-3.11-Linux-x86_64-Install.gz 
+        gunzip AESCrypt-GUI-3.11-Linux-x86_64-Install.gz 
+        chmod +x AESCrypt-GUI-3.11-Linux-x86_64-Install 
+        ./AESCrypt-GUI-3.11-Linux-x86_64-Install
+        rm AESCrypt-GUI-3.11-Linux-x86_64-Install
+fi
